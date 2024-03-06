ee526034b55c275ddb76e896a0a7d8dfb7cb47a9
Victor-Lis <victorlisbronzo1@gmail.com>
2024-03-06 10:24:30 -0300

    v1
    - Iniciando projeto e testando push button com 2 pinos

---
+++ timer.ino
@@ -0,0 +1,19 @@
+int botao1 = 3;
+int led = 4;
+
+void setup() {
+  pinMode(botao1, INPUT);
+  pinMode(led, OUTPUT);
+  Serial.begin(9600);
+}
+
+void loop() {
+  boolean isPushed = !digitalRead(botao1);
+  Serial.println(isPushed);
+  if(isPushed){
+    digitalWrite(led, HIGH);
+  }else{
+    digitalWrite(led, LOW);
+    delay(500);
+  }
+}
